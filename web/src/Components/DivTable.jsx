3f01ba34b082b50c21dfaea147b41ccc39d7a33c
kelly <114171338+kellydaihanariostorres@users.noreply.github.com>
2023-11-26 18:56:24 -0500

    vista a medias de bodega

---
+++ web/src/Components/DivTable.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const DivTable = ({children, col, off, classLoad, classTable}) => {
+  return (
+    <div className='rpw mt-3'>
+      <div className={'col-md'+col+'offset-md.'+off}>
+        <div className={'card border-wite text-center'+classLoad}>
+          <img src='/loading.gif' className='img-fluid'></img>
+        </div>
+      </div>
+      <div className={'table-responsive'+ classTable}>
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default DivTable
--- web/src/functions.jsx
+++ web/src/functions.jsx
@@ -1,43 +1,51 @@
-import Swal from "sweetalert2";
-import storage from "./Storage/storage";
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import storage from './Storage/storage';
 
-export const show_alerta = (msj,icon)=> {
-    Swal.fire({title:msj, icon:icon, buttonsStyling:true});
-}
+export const show_alerta = (msj, icon) => {
+  Swal.fire({ title: msj, icon: icon, buttonsStyling: true });
+};
 
-export const sendRequest = async(method,params,url,redir='',token=true)=>{
-    if (token){
-        const authToken = storage.get('authToken');
-        axios.defaults.headers.common['Authorization']= 'Bearer' + authToken;
-    }
-    let res;
-    await axios ({method:method, url:url, data:params}).then(
-        response =>{
-            res = response.data,
-            (method != 'GET')? show_alerta(response.data.message,'success'):'',
-            setTimeout(()=>
-            (redir !=='')? window.location.href = redir : '', 200)
-        }).catch((errors) =>{
-            let desc='';
-            res = errors.response.data,
-            errors.response.data.errors,map( (e) => {desc=desc+ ' '+ e})
-            show_alerta(desc,'error')
-        })
-    return res;
-}
+export const sendRequest = async (method, params, url, redir = '', token = true) => {
+  if (token) {
+    const authToken = storage.get('authToken');
+    axios.defaults.headers.common['Authorization'] = 'Bearer ' + authToken;
+  }
 
-export const confirmation =async(name,url,redir)=>{
-    const alert =Swal.mixin({buttonsStyling:true});
-    alert.fire({
-        title:'Are you sure delete'+name+'?',
-        icon :'question',showCancelButton:true,
-        confirmButtonText:'<i class="fa-solid fa-check></i> yes, delete',
-        cancelButtonText:'<i class="fa-solid fa-ban></i> cancel',
-    }).then((result) =>{
-        if(result.isConfirmed){
-            sendRequest('DELETE',{},url,redir);
-        }
+  let res;
+  await axios({ method: method, url: url, data: params })
+    .then((response) => {
+      res = response.data;
+      if (method !== 'GET') show_alerta(response.data.message, 'success');
+      setTimeout(() => (redir !== '') ? (window.location.href = redir) : '', 200);
     })
-}
+    .catch((errors) => {
+      let desc = '';
+      res = errors.response.data;
+      errors.response.data.errors.forEach((e) => {
+        desc = desc + ' ' + e;
+      });
+      show_alerta(desc, 'error');
+    });
+
+  return res;
+};
+
+export const confirmation = async (name, url, redir) => {
+  const alert = Swal.mixin({ buttonsStyling: true });
+  alert
+    .fire({
+      title: 'Are you sure to delete ' + name + '?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: '<i class="fa-solid fa-check"></i> yes, delete',
+      cancelButtonText: '<i class="fa-solid fa-ban"></i> cancel',
+    })
+    .then((result) => {
+      if (result.isConfirmed) {
+        sendRequest('DELETE', {}, url, redir);
+      }
+    });
+};
 
 export default show_alerta;
