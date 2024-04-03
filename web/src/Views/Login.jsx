da7158e0e918156aa1896322080b050d51ad40c1
kelly <114171338+kellydaihanariostorres@users.noreply.github.com>
2024-04-03 01:00:39 -0500

    estilo de login

--- web/src/Views/Login.jsx
+++ web/src/Views/Login.jsx
@@ -45,9 +45,9 @@ const Login = () => {
     <div className="full-height d-flex justify-content-center align-items-center text-white">
       <div className="row mt-5 flex-grow-1">
         <div className="col-md-4 offset-md-4">
-          <div className="card ">
+          <div className="cardt ">
             <div className="card-header text-white text-center">Iniciar Sesión</div>
-            <img src='../../public/logo.jpg' alt="Imagen Descriptiva" className="logo-img mt-3"/>
+            <img src='../../public/logo.jpg' alt="Imagen Descriptiva" className="logo-imgt mt-3"/>
             <div className="card-body ">
               {error && <div className="alert alert-danger">{error}</div>} {/* Mostrar el mensaje de error si existe */}
               <form onSubmit={login}>
@@ -55,7 +55,9 @@ const Login = () => {
                 <input type="text" className="form-control mb-3" placeholder="Nombre de usuario" value={userName} onChange={(e) => setUserName(e.target.value)}  style={{ backgroundColor: '#1a1a1a', color: '#ffffff', border: '1px solid #1a1a1a',boxShadow: '0px 1px 0px 0px #ffffff',}}/>
                 <h7 className= 'text'>Contraseña</h7>
                 <input type="password" className="form-control mb-3" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)}  style={{ backgroundColor: '#1a1a1a', color: '#ffffff', border: '1px solid #1a1a1a',boxShadow: '0px 1px 0px 0px #ffffff',}}/>
-                <button type="submit" className="btn btn-primary btn-block">Iniciar Sesión</button>
+                
+                
+                <button type="submit" className="btn btn-primaryte btn-block" style={{backgroundColor:'#4c0101', color:'white', justifyContent:'center',margin:'0 auto',display:'block',}}>Iniciar Sesión</button>
               </form>
             </div>
           </div>
